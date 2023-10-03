e6848569222ca831b06893ad42f4638d0dc0e25f
gisellerx <giselle.armas@outlook.com>
2023-10-03 18:01:16 -0400

    added searchCity and displayWeather functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,29 @@
 var apiKey = "3b104c2232eb817eda16cdfc92cbe1af"
-var titleEl = document.getElementById("title");
-var tempEl = document.getElementById("temp");
-var windEl = document.getElementById("wind");
-var humidityEl = document.getElementById("humidity");
-var searchBtn = document.getElementById("search-btn");
-var cityInput = document.getElementById("city-input");
+var titleEl = document.getElementById("title")
+var tempEl = document.getElementById("temp")
+var windEl = document.getElementById("wind")
+var humidityEl = document.getElementById("humidity")
+var searchBtn = document.getElementById("search-btn")
+var cityInput = document.getElementById("city-input")
+
+function searchCity(event) {
+    event.preventDefault()
+    var cityName = cityInput.value
+
+    displayWeather(cityName)
+}
+
+function displayWeather(cityName) {
+    var url = "https://api.openweathermap.org/data/2.5/weather?q="+cityName+"&appid="+ apiKey+"&units=imperial"
+
+    fetch(url)
+    .then(function(response) {
+        return response.json()
+    })
+    .then(function(currentData) {
+        console.log(currentData)
+    })
+}
+
+searchBtn.addEventListener("click", searchCity)
 
