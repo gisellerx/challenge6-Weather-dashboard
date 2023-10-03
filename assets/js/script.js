a4f2a7654084cf0c201afaee670005c4c14b29cc
gisellerx <giselle.armas@outlook.com>
2023-10-03 17:43:22 -0400

    added vars tp js

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+var apiKey = "3b104c2232eb817eda16cdfc92cbe1af"
+var titleEl = document.getElementById("title");
+var tempEl = document.getElementById("temp");
+var windEl = document.getElementById("wind");
+var humidityEl = document.getElementById("humidity");
+var searchBtn = document.getElementById("search-btn");
+var cityInput = document.getElementById("city-input");
+
